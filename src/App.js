c6a702a9feaf70f03796589247ad822994ed9982
stagiaire <stagiaire@cpg-453070.cepegra2008.lan>
2019-05-03 16:34:44 +0200

    Final stage of the demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      
+      <header>
+        <Link to="/">Section 1</Link> | 
+        <Link to="/2">Section 2</Link> | 
+        <Link to="/3">Section 3</Link>
       </header>
+
+      <Switch>  {/* Assure qu'il n'y aura qu'une seule route active. Bonne pratique, même si pas réellement besoin pour un exact path avec des routes différentes */}
+      <Route exact path="/" render={
+        () => (
+          <section>
+            <h2>SECTION 1</h2>
+          </section>
+        )
+      }/>
+
+      <Route exact path="/2" render={
+        () => (
+          <section>
+            <h2>SECTION 2</h2>
+          </section>
+        )
+      }/>
+
+      <Route exact path="/3" render={
+        () => (
+          <section>
+            <h2>SECTION 3</h2>
+          </section>
+        )
+      }/>
+      </Switch>
+
+      </Router>
     </div>
   );
 }
